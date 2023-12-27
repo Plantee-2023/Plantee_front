03adcf6bc83f84965e8c7d376de13671047381de
Arum <rlfehd429@gmail.com>
2023-12-27 13:15:39 +0900

    Feat: First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Menu from './Main/Menu';
+import Main from './Main/Main';
+import './App.css';
+import Foot from './Main/Foot';
+import Header from './Main/Header';
+
+function App() {
+	return (
+		<div className="App">
+			<Header/>
+			<Menu/>
+			<Main/>
+			<Foot/>
+		</div>
+	);
+}
+
+export default App;
