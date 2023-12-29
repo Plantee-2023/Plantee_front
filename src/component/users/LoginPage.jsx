03936bc6a2403c553dcd26c816a24b81bcf0bd66
kim00bang <wkekrk1227@naver.com>
2023-12-29 15:27:18 +0900

    chore : 수저저정

--- src/component/users/LoginPage.jsx
+++ src/component/users/LoginPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginPage = () => {
     return (
         <div>
-            <h1 className='text-center mt-5'>로그인 페이지</h1>
+            <h1 className='text-center mt-5'>로그인 페이지이이이</h1>
         </div>
     )
 }
