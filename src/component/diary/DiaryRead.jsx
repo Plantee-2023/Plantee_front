9b35d9fd438784d7490181af82eb6c8da913a98e
minji12344 <alswl12344@naver.com>
2024-01-03 19:35:58 +0900

    Test : test

--- src/component/diary/DiaryRead.jsx
+++ src/component/diary/DiaryRead.jsx
@@ -11,40 +11,6 @@ const DiaryRead = () => {
                     <h2 className='mt-5'><b>식물이름</b></h2>
                 </div>
                 <div className='mt-5 text-center'>
-                    <Card style={{ width: '40rem' }}>
-                        <Card.Body>
-                            <h5>함께한지 0 일이 지났어요</h5>
-                            <hr />
-                            <Row>
-                                <Col>
-                                    <h5>물 주기</h5>
-                                </Col>
-                                <Col>
-                                    <h5>마지막 물 준 날</h5>
-                                </Col>
-                                <Col>
-                                    <h5>처음 함께한 날</h5>
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '40rem' }} className='mt-2'>
-                        <h5>D-Day</h5>
-                        <Row>
-                            <Col>
-                                <h5>D-5</h5>
-                                <p>물</p>
-                            </Col>
-                            <Col>
-                                <h5>D-5</h5>
-                                <p>영양제</p>
-                            </Col>
-                            <Col>
-                                <h5>D-7</h5>
-                                <p>분갈이</p>
-                            </Col>
-                        </Row>
-                    </Card>
                 </div>
                 <div className='mt-5'>
                     <Card style={{ width: '40rem' }}>
