03d4c620eb04f1cf319b82d8d4d0ce07806497ac
Arum <rlfehd429@gmail.com>
2024-01-03 18:58:01 +0900

    Feat: 정렬 기능 구현중

--- src/component/store/StoreMain.jsx
+++ src/component/store/StoreMain.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Button, Container, Form, Nav, Navbar, NavDropdown, InputGroup } from 'react-bootstrap';
 import "./Store.css";
 
 const StoreMain = () => {
@@ -12,19 +13,21 @@ const StoreMain = () => {
             <div className='store_wrap'>
                 <div className='store_contents'>
 
-                    <div className='store_filterbtn_group'>
 
-                        <button className='button_hide' onClick={() => { setClickPlant((e1) => !e1); }} >
+
+                    <div className='store_filterbtn_group'>
+                        <button className='plant_filterbtn'>↺</button>
+                        <button className='button_hide' onClick={() => { setClickPlant((e) => !e); }} >
                             <div className={`${!isClickPlant ? 'store_filterbtn' : 'store_filterbtn_clicked'}`} >
-                                <div style={{ verticalAlign: 'middle' }}>식물</div>
+                                <div>식물</div>
                             </div>
                         </button>
-                        <button className='button_hide' onClick={() => { setClickGoods((e2) => !e2); }} >
+                        <button className='button_hide' onClick={() => { setClickGoods((e) => !e); }} >
                             <div className={`${!isClickGoods ? 'store_filterbtn' : 'store_filterbtn_clicked'}`} >
-                                <div style={{ verticalAlign: 'middle' }}>용품</div>
+                                <div>용품</div>
                             </div>
                         </button>
-                        
+
                     </div>
 
 
@@ -46,6 +49,31 @@ const StoreMain = () => {
                             </div>
                         </>
                     )}
+
+
+                    <div>
+                        <Navbar bg="#ffffff" data-bs-theme="light" className='pt-3 pb-3'>
+                            <Container fluid>
+                                <Navbar.Collapse id="navbarScroll">
+                                    <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll >
+                                        <NavDropdown title="정렬" id="navbarScrollingDropdown">
+                                            <NavDropdown.Item href="#action1">최신순</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action2">인기순</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action3">리뷰많은순</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action4">낮은가격순</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action5">높은가격순</NavDropdown.Item>
+                                        </NavDropdown>
+                                    </Nav>
+                                    <form>
+                                        <InputGroup className='store_searchinputwrap'>
+                                            <input type='search' className='store_searchinput' placeholder='검색어를 입력해주세요.' />
+                                            <button className='store_searchbtn' type='submit'><img src='/image/search_icon.png' /></button>
+                                        </InputGroup>
+                                    </form>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                    </div>
 
 
 
