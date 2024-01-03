7750d74299e4c8d30ffe1cf4cd320a9593406f7e
minji12344 <alswl12344@naver.com>
2024-01-03 21:34:06 +0900

    Feat : 캘린더 가져오기

--- src/component/diary/DiaryList.jsx
+++ src/component/diary/DiaryList.jsx
@@ -15,8 +15,7 @@ const DiaryList = () => {
                     <div className='mt-5 text-center'>
                         <h1><b>목록</b></h1>
                     </div>
-                    <div className='text-center mt-5'>
-                        <div className='line'>
+                    <div className='line'>
                             <h3>물주기</h3>
                             <Card style={{ width: '30rem' }}>
                                 <CardBody>
@@ -39,30 +38,6 @@ const DiaryList = () => {
                                 </CardBody>
                             </Card>
                         </div>
-                        <div className='line mt-5'>
-                            <h3>영양제</h3>
-                            <Card style={{ width: '30rem' }}>
-                                <CardBody>
-                                    <Row>
-                                        <Col>
-                                            <img src="http://via.placeholder.com/100x100" alt='plante' />
-                                        </Col>
-                                        <Col>
-                                            <form>
-                                                식물이름
-                                            </form>
-                                            <form>
-                                                <img src="http://via.placeholder.com/50x50" alt='plante' />
-                                            </form>
-                                            <form>
-                                                메모장?
-                                            </form>
-                                        </Col>
-                                    </Row>
-                                </CardBody>
-                            </Card>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
