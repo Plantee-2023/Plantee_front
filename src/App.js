bf3c0b747e4cb0c80194539cc27f047acad360a5
kim00bang <wkekrk1227@naver.com>
2024-01-04 11:22:12 +0900

    feat : 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import Menu from './component/Menu';
 import RouterPage from './component/RouterPage';
 import Footer from './component/Footer';
 import './App.css';
-
-import './App.css';
-
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { BoxContext } from './component/common/BoxContext'
+import BoxModal from './component/common/BoxModal'
 function App() {
+	const [box, setBox] = useState({
+		show: false,
+		message: "",
+		action: null
+	});
 	return (
-		<div className='main_contents'>
-			<Menu/>
-			<RouterPage/>
-			<Footer/>
-		</div>
+		<BoxContext.Provider value={{ box, setBox }}>
+			<Container>
+				<div className='main_contents'>
+					<Menu />
+					<RouterPage />
+					<Footer />
+					{box.show && <BoxModal />}
+				</div>
+			</Container>
+		</BoxContext.Provider>
 	);
 }
 
--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Main.css'
-import { InputGroup, NavDropdown, Navbar, Nav } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { InputGroup, NavDropdown, Navbar, Nav, Toast, CloseButton, Button, Card } from 'react-bootstrap'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { LiaStoreAltSolid } from "react-icons/lia";
 import { PiUserListBold } from "react-icons/pi";
 import { GiTalk } from "react-icons/gi";
 import { FiBookOpen } from "react-icons/fi";
 import { SlNotebook } from "react-icons/sl";
+import { BoxContext } from './common/BoxContext';
 
 const Menu = () => {
+    const [showA, setShowA] = useState(false);
+    const toggleShowA = () => setShowA(!showA);
+    const { box, setBox } = useContext(BoxContext);
+    const navigate = useNavigate();
+    const onLogout = () => {
+        setBox({
+            show: true,
+            message: "로그아웃 하시겠습니까?",
+            action: () => {
+                sessionStorage.clear();
+                navigate("/");
+            }
+        });
+    }
     return (
         <div className='menu_wrap'>
             <div className='menu_contents'>
-
                 <div className='menu_header'>
                     <div className='menu_left'>
                         <h1 className='menu_logo'>
@@ -29,35 +43,55 @@ const Menu = () => {
                     </div>
                     <div className='menu_right'>
                         <ul className='menu_mymenu'>
-                            <li><NavLink to='/users/myPage'>마이페이지</NavLink></li>
-                            <li><NavLink to='/users/LoginPage'>로그인</NavLink></li>
-                            <li><NavLink to='/users/Join'>회원가입</NavLink></li>
+                            {!sessionStorage.getItem("uid") ?
+                                <>
+                                    <li><NavLink to='/users/LoginPage'>로그인</NavLink></li>
+                                    <li><NavLink to='/users/Join'>회원가입</NavLink></li>
+                                </>
+                                :
+                                <>
+                                    <li>
+                                        <div onClick={toggleShowA} style={{ cursor: 'pointer' }}>{sessionStorage.getItem("uid")}님
+                                            <Toast show={showA}>
+                                                <CloseButton />
+                                                <Toast.Body>
+                                                    <Button>내 정보</Button>
+                                                    <Card className='menu-card'>캘린더</Card>
+                                                    <Card className='menu-card'>나의 레시피</Card>
+                                                    <Card className='menu-card'>내가 등록한글</Card>
+                                                    <Card className='menu-card'>좋아요 목록</Card>
+                                                </Toast.Body>
+                                            </Toast>
+                                        </div>
+                                    </li>
+                                    <li><NavLink onClick={onLogout}>로그아웃</NavLink></li>
+                                </>
+                            }
                         </ul>
                     </div>
                 </div>
-                <Navbar expand="lg" className="mt-5 recommend">
+                <Navbar expand="lg" className="menu-back-color">
                     <Nav>
-                        <PiUserListBold className='icon ms-5' />
-                        <NavDropdown className='text-color' title="메뉴유" id="basic-nav-dropdown">
+                        <PiUserListBold className='menu-icon' />
+                        <NavDropdown className='menu-text-color' title="메뉴유" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/plant/dictionary">식물백과</NavDropdown.Item>
                             <NavDropdown.Item href="">식물 큐레이트</NavDropdown.Item>
                             <NavDropdown.Item href="">레시피</NavDropdown.Item>
                             <NavDropdown.Item href="">지도</NavDropdown.Item>
                         </NavDropdown>
-                        <GiTalk className='icon ms-5' />
-                        <NavDropdown className='text-color' title="커뮤니티" id="basic-nav-dropdown">
+                        <GiTalk className='menu-icon' />
+                        <NavDropdown className='menu-text-color' title="커뮤니티" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/comm">자유게시판</NavDropdown.Item>
                             <NavDropdown.Item href="">내 식물 자랑</NavDropdown.Item>
                             <NavDropdown.Item href="">나눔</NavDropdown.Item>
-                            <NavDropdown.Item href="">자유게시판</NavDropdown.Item>
                             <NavDropdown.Item href="">Q&A</NavDropdown.Item>
                         </NavDropdown>
-                        <LiaStoreAltSolid className='icon ms-5' />
+                        <LiaStoreAltSolid className='menu-icon' />
                         <NavLink className='menu-magazine' to='/store/main'>스토어</NavLink>
-                        <FiBookOpen className='icon ms-5' />
+                        <FiBookOpen className='menu-icon' />
                         <NavLink className='menu-magazine' to='/main/magazineList'>매거진</NavLink>
                     </Nav>
                 </Navbar>
-
             </div>
         </div>
     )
