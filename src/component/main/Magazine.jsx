76c00193fbba961fcc3fe788a29a65e7cd3f5d09
kim00bang <wkekrk1227@naver.com>
2024-01-05 17:33:22 +0900

    chore : 수정

--- src/component/main/Magazine.jsx
+++ src/component/main/Magazine.jsx
@@ -16,14 +16,16 @@ const Magazine = () => {
 
     if (loading) return <div className='text-center'><Spinner size='lg' /></div>
     return (
-        <div>
-            <h1 className='magazine-title'>얘는 무슨 식물이냐?</h1>
-            <hr />
-            <div className='magazine-count'>조회수 : 0</div>
-            <div className='magazine-img'>
-                <img src="/image/what1.jpg" width={900} height={600} />
+        <div id="main_wrap">
+            <div className="main_contents">
+                <h1 className='magazine-title'>얘는 무슨 식물이냐?</h1>
+                <hr />
+                <div className='magazine-count'>조회수 : 0</div>
+                <div className='magazine-img'>
+                    <img src="/image/what1.jpg" width={900} height={600} />
+                </div>
+                <h5 className='magazine-text'>얘는 무슨 식물이냐?</h5>
             </div>
-            <h5 className='magazine-text'>얘는 무슨 식물이냐?</h5>
         </div>
     )
 }
