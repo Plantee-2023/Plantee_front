ccde4d37d34798c01bc36e94860dd3e6fc4fc7d9
minji12344 <alswl12344@naver.com>
2024-01-09 18:42:42 +0900

    Chose: 수정중

--- src/component/diary/DiaryList.jsx
+++ src/component/diary/DiaryList.jsx
@@ -38,47 +38,51 @@ const DiaryList = () => {
                 <div className='mt-5 text-center'>
                     <h1><b>앨범</b></h1>
                 </div>
-                <div className='text-end mt-3' onClick={() => { onClickInsert()}}>
-                    <img src='/image/icon-add.png' className='diary-img-insert'/><span className='diary-insert-size'><b><u>등록하기</u></b></span>
+                <div className='text-end mt-3' onClick={() => { onClickInsert() }}>
+                    <img src='/image/icon-add.png' className='diary-img-insert' /><span className='diary-insert-size'><b><u>등록하기</u></b></span>
                 </div>
                 <div className='list_all'>
                     <div className='line_top1'>
                         <img src='/image/icon_water.png' width={'40px'} height={'40px'} />
+                        <span> 물 주기</span>
                     </div>
                     <div className='line_1'>
                         <h2 className='mb-5'>물 주기</h2>
                         <div className='diarylist_cardgroup'>
                             {list.map(d =>
                                 <div>
-                                    <Card style={{ width: '40rem' }} className='diarylist_card'>
-                                        <CardBody>
-                                            <Row>
-                                                <Col md={4}>
-                                                    <img src={d.image} width={150} height={150} />
-                                                </Col>
-                                                <Col className='list_col'>
-                                                    <h3>
-                                                        {d.plant_name}
-                                                    </h3>
-                                                    <form>
-                                                        {d.fmtdate}
-                                                    </form>
-                                                </Col>
-                                            </Row>
-                                        </CardBody>
-                                    </Card>
+                                    {d.date_water > -3 && (
+                                        <Card style={{ width: '40rem' }} className='diarylist_card'>
+                                            <CardBody>
+                                                <Row>
+                                                    <Col md={4}>
+                                                        <img src={d.image} width={150} height={150} />
+                                                    </Col>
+                                                    <Col className='list_col'>
+                                                        <h3>
+                                                            {d.plant_name}
+                                                        </h3>
+                                                        <form>
+                                                            {d.fmtdate}
+                                                        </form>
+                                                    </Col>
+                                                </Row>
+                                            </CardBody>
+                                        </Card>
+                                    )}
                                 </div>
                             )}
                         </div>
                     </div>
                     <div className='line_top2'>
-                        <img src='/image/icon_sun.png' width={'60px'} height={'60px'} />
+                        <img src='/image/icon_soild.png' width={'60px'} height={'60px'} />
                     </div>
                     <div className='line_2'>
-                        <h2 className='mb-5'>햇빛 주기</h2>
+                        <h2 className='mb-5'>분갈이 해주기</h2>
                         <div className='diarylist_cardgroup'>
                             {list.map(d =>
                                 <div>
+                                    {d.date_medicine > -3 && (
                                     <Card style={{ width: '40rem' }} className='diarylist_card'>
                                         <CardBody>
                                             <Row>
@@ -96,47 +100,20 @@ const DiaryList = () => {
                                             </Row>
                                         </CardBody>
                                     </Card>
+                                    )}
                                 </div>
                             )}
                         </div>
                     </div>
                     <div className='line_top3'>
-                        <img src='/image/icon_soild.png' width={'60px'} height={'60px'} />
-                    </div>
-                    <div className='line_3'>
-                        <h2 className='mb-5'>분갈이 해주기</h2>
-                        <div className='diarylist_cardgroup'>
-                            {list.map(d =>
-                                <div>
-                                    <Card style={{ width: '40rem' }} className='diarylist_card'>
-                                        <CardBody>
-                                            <Row>
-                                                <Col md={4}>
-                                                    <img src={d.image} width={150} height={150} />
-                                                </Col>
-                                                <Col className='list_col'>
-                                                    <h3>
-                                                        {d.plant_name}
-                                                    </h3>
-                                                    <form>
-                                                        {d.fmtdate}
-                                                    </form>
-                                                </Col>
-                                            </Row>
-                                        </CardBody>
-                                    </Card>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                    <div className='line_top4'>
                         <img src='/image/icon_medicine.png' width={'60px'} height={'60px'} />
                     </div>
-                    <div className='line_4'>
+                    <div className='line_3'>
                         <h2 className='mb-5'>영양제 주기</h2>
                         <div className='diarylist_cardgroup'>
                             {list.map(d =>
                                 <div>
+                                    {d.date_change > -3 && (
                                     <Card style={{ width: '40rem' }} className='diarylist_card'>
                                         <CardBody>
                                             <Row>
@@ -154,6 +131,7 @@ const DiaryList = () => {
                                             </Row>
                                         </CardBody>
                                     </Card>
+                                    )}
                                 </div>
                             )}
                         </div>
--- src/component/diary/DiaryUpdate.jsx
+++ src/component/diary/DiaryUpdate.jsx
@@ -1,49 +1,20 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { Button, InputGroup, Form } from 'react-bootstrap'
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useRef } from 'react'
+import { Button, InputGroup, Form, Dropdown, DropdownButton } from 'react-bootstrap'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
 const DiaryInsert = () => {
     const img_ref = useRef(null);
-    const navi = useNavigate();
-    const [plants, setplants] = useState([]);
-    //const [p_value, setP_value] = useState("");
 
-    //const [diary, setDiary] = useState({
-    //    diary_id: "", user_id: "", image: "", contents: "", reg_date: "", last_watering: "", watering: "", common_name: "", date_now:"", date_water:"", date_medicine:"", date_change:"",
-    //})
 
-    // const { user_id, plant_name, image, contents, reg_date, fmtdate, waterdate, watering, common_name, date_now, date_water, date_medicine,date_change } = diary;
-
-    const { plant_id } = useParams();
-
-    const [plant, setPlant] = useState({
-        plant_id: '', common_name: '', image: '', contents: '', watering: '', sunlight: '', care_level: '', leaf: '',
-        flowers: '', fruits: '', type: '', indoor: '', poisonous_pet: '', cuisine: ''
-      });
-    
-      const { common_name, image, contents, watering, sunlight, care_level, leaf, flowers, fruits, type, indoor, poisonous_pet, cuisine } = plant;
-
-    const getList = async () => {
-        const res = await axios.get(`/plant/list.json`);
-        setplants(res.data.list);
-        console.log(plant);
+    const onClickReset =()=>{
+        
     }
-
-    //const selectValue = ()=>{
-    //    {value:{plant_id}}
-    //}
-
-    useEffect(() => {
-        getList();
-    }, []);
-
     return (
         <div className='plant_wrap'>
             <div className='plant_contents'>
                 <div className='text-center'>
-                    <h1 className='mt-5'>나의 식물 등록하기</h1>
+                    <h1 className='mt-5'>나의 식물 수정하기</h1>
                     <div className='mt-5'>
                         <img src="http://via.placeholder.com/250x250" onClick={() => img_ref.current.click()} style={{ cursor: 'pointer' }} />
                         <input type='file' ref={img_ref} style={{ display: 'none' }} />
@@ -52,36 +23,40 @@ const DiaryInsert = () => {
                         <div>
                             <InputGroup className='diary-input'>
                                 <InputGroup.Text className='diary-text'>식물 이름</InputGroup.Text>
-                                <Form.Control type='text' />
+                                <Form.Control name='uname' type='text' />
                             </InputGroup>
                             <InputGroup className='diary-input'>
                                 <InputGroup.Text className='diary-text'>식물 종류</InputGroup.Text>
                                 <Form.Select className='select_box'>
-                                    {plants.map(p =>
-                                        <option value={p.plant_id} className='text-center'>{p.common_name}</option>
-                                    )}
+                                    <option>화분 선택 (*옵션)</option>
+                                    <option>스투키</option>
+                                    <option>알로에</option>
+                                    <option>선인장</option>
                                 </Form.Select>
                             </InputGroup>
                             <InputGroup className='diary-input'>
                                 <InputGroup.Text className='diary-text'>분류</InputGroup.Text>
-                                <Form.Control type='text' readOnly />
+                                <Form.Control name='uname' type='text' />
                                 <InputGroup.Text className='diary-text'>난이도</InputGroup.Text>
-                                <Form.Control type='text' />
+                                <Form.Control name='uname' type='text' />
                             </InputGroup>
                             <InputGroup className='diary-input'>
                                 <InputGroup.Text className='diary-text'>물 주기</InputGroup.Text>
-                                <Form.Control type='text' />
+                                <Form.Control name='uname' type='text' />
                                 <InputGroup.Text className='diary-text'>햇빛 주기</InputGroup.Text>
-                                <Form.Control type='text' />
+                                <Form.Control name='uname' type='text' />
                             </InputGroup>
-
+                            <InputGroup className='diary-input'>
+                                <InputGroup.Text className='diary-text'>수정일</InputGroup.Text>
+                                <Form.Control name='uname' type='text' />
+                            </InputGroup>
                             <InputGroup className='diary-input-memo'>
                                 <Form.Control name='uname' type='text' placeholder='메모장' />
                             </InputGroup>
                         </div>
                         <div className='text-center'>
-                            <button className='mt-4 diary-add'>등록</button>
-                            <button className='mx-2 diary-add' type='reset'>취소</button>
+                            <button className='mt-4 diary-add'>수정</button>
+                            <button className='mx-2 diary-add'>취소</button>
                         </div>
                     </div>
                 </div>
