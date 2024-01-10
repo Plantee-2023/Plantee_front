62dba8593d66faf60e80063e93fd3703c12b64e2
gwoneu <gwoneunji9634@gmail.com>
2024-01-10 18:33:48 +0900

    Feat: 레시피 리스트, 읽기 기능구현

--- src/component/plant/PlantRecipe.jsx
+++ src/component/plant/PlantRecipe.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { BoxContext } from '../common/BoxContext';
+import { useNavigate } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+import axios from 'axios';
 
 const PlantRecipe = () => {
+
+  const [loading, setLoading] = useState(false);
+  const navi = useNavigate();
+  const { box, setBox } = useContext(BoxContext);
+  const [recipe, setrecipe] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  // 레시피 글쓰기 버튼 클릭 시
+  const onClickInsertRecipe = () => {
+    if (sessionStorage.getItem("uid")){
+      navi('/recipe/insert');
+    } else {
+      setBox({ show: true, message: "로그인 사용자만 이용 가능한 서비스 입니다. 로그인 후 진행해주세요." })
+            sessionStorage.setItem("target", location.pathname);
+            navi("/users/loginPage");
+    }
+  }
+
+  const getList = async () => {
+    setLoading(true)
+    const res = await axios.get(`/recipe/list.json`);
+    setrecipe(res.data.list)
+    setTotal(res.data.total)
+    setLoading(false);
+  }
+
+  useEffect(() =>{
+    getList();
+  }, []);
+
+  if (loading) return <div className='text-center my-5'><Spinner animation="border" variant="success" /></div>
+
   return (
     <div className='recipe_wrap'>
       <div className='recipe_contents'>
@@ -18,85 +53,31 @@ const PlantRecipe = () => {
             </div>
           </div>
           <div className='recipe_btn'>
-            <Link to="/recipe/insert">
-            <button className='recipe_insert'>글쓰기</button>
-            </Link>
+            <button className='recipe_insert' onClick={()=>onClickInsertRecipe()}>글쓰기</button>
           </div>
+        </div>
+
+        <div className='recipe_total_section'>
+          <span>총 레시피 수 : <strong>{total}</strong></span>
         </div>
 
         <div className='recipe_contents_section'>
           <div className='recipe_contents_grid'>
-            <a href='/plant/recipe/read'>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_level'><img src='/image/recipe_level.png'/>초급</span>
-                    <span className='recipe_like'><img src='/image/like_icon.png'/> 4</span>
+            {recipe.map(r => 
+              <a href={`/recipe/read/${r.recipe_id}`}>
+                <div className='recipe_content_item'>
+                  <img src='/image/recipe_01.jpg'/>
+                  <p className='recipe_title'>{r.title}</p>
+                  <p className='recipe_writer'>작성자</p>
+                    <div className='recipe_content_level'>
+                      <span className='recipe_level'><img src='/image/recipe_level.png'/>초급</span>
+                      <span className='recipe_like'><img src='/image/like_icon.png'/> 4</span>
+                  </div>
                 </div>
-              </div>
-            </a>
-
-            <a href=''>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_leve'>초급</span>
-                </div>
-              </div>
-            </a>
-
-            <a href=''>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_leve'>초급</span>
-                </div>
-              </div>
-            </a>
-
-            <a href=''>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_leve'>초급</span>
-                </div>
-              </div>
-            </a>
-
-            <a href=''>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_leve'>초급</span>
-                </div>
-              </div>
-            </a>
-
-            <a href=''>
-              <div className='recipe_content_item'>
-                <img src='/image/recipe_01.jpg'/>
-                <p className='recipe_title'>레시피 타이틀</p>
-                <p className='recipe_writer'>작성자</p>
-                  <div className='recipe_content_level'>
-                    <span className='recipe_leve'>초급</span>
-                </div>
-              </div>
-            </a>
-
+              </a>
+            )}
           </div>
-
         </div>
-
       </div>
     </div>
   )
