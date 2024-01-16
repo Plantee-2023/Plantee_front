e342769e4abca2a55fb28754e6d9d5359763cf29
minji12344 <alswl12344@naver.com>
2024-01-16 14:40:05 +0900

    Feat : 회원정보 구현

--- src/component/mypage/detail/UserUpdate.jsx
+++ src/component/mypage/detail/UserUpdate.jsx
@@ -1,73 +1,106 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, useContext } from 'react'
 import axios from 'axios'
 import { Card, Form, InputGroup, Button, Spinner } from 'react-bootstrap'
+import { BoxContext } from '../../common/BoxContext';
+import { useNavigate } from 'react-router-dom';
 
 const UserUpdate = () => {
+    const navi = useNavigate();
+    const img_ref = useRef(null);
+    const [users, setUsers] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { box, setBox } = useContext(BoxContext);
+    const [user, setUser] = useState({
+        nickname: '',
+        image: '',
+        phone: '',
+        address1: '',
+        address2: '',
+    })
 
-  const img_ref = useRef(null);
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState({
-      nickname: '',
-      photo: '',
-      phone: '',
-      address1: '',
-      address2: '',
-      reg_date: ''
-  })
+    const { uid ,nickname, image, phone, address1, address2 } = user;
 
-  const {nickname,image,phone,address1,address2,reg_date} = user;
-  const getList = async() => {
-      setLoading(true);
-      const res = await axios.get(`/users/read.json/${sessionStorage.getItem("uid")}`);
-      console.log(res.data)
-      setUsers(res.data.list);
-      setLoading(false);
-  }
-  useEffect(() => {
-      getList();
-  },[]);
+    const onChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+        //console.log(user);
+    }
 
-  if (loading) return <div className='text-center'><Spinner /></div>
-  
-  return (
-    <div>
-      <Card className='update-card p-5'>
-          <h1 className='all-title'>정보 수정</h1>
-          <div className='update_img'>
-              <img src='http://via.placeholder.com/150x150' onClick={() => img_ref.current.click()} style={{ cursor: 'pointer' }} width={300} height={300} />
-              <input type='file' ref={img_ref} style={{ display: 'none' }} />
-              <br />
-              <Button className='update-img-btn'>이미지 수정</Button>
-          </div>
-          <InputGroup className='join-input'>
-              <InputGroup.Text className='update-text'>닉네임</InputGroup.Text>
-              <Form.Control value={nickname} name='nickname' type='text' />
-          </InputGroup>
-          <InputGroup className='join-input'>
-              <InputGroup.Text className='update-text'>연락처</InputGroup.Text>
-              <Form.Control value={phone} name='phone' type='text' />
-          </InputGroup>
-          <InputGroup className='join-address'>
-              <InputGroup.Text className='update-text'>주소</InputGroup.Text>
-              <Form.Control value={address1} name='address1' type='text' />
-              <button className='update-btn'>검색</button>
-          </InputGroup>
-          <Form.Control value={address2} name='address2' className='join-input' type='text' placeholder='상세주소' />
-          <Form className='join-check'>
-              <Form.Check label="꽃집 사장님"></Form.Check>
-          </Form>
-          <InputGroup className='join-input'>
-              <InputGroup.Text className='update-text'>사업자 등록증</InputGroup.Text>
-              <Form.Control />
-              <button className='update-btn'>파일 검색</button>
-          </InputGroup>
-      </Card>
-      <div className='text-center'>
-          <button className='mt-4 join-add'>정보수정</button>
-      </div>
-    </div>
-  )
+    const onSubmit = () => {
+        setBox({
+            show: true,
+            message: "회원 정보를 수정하시겠습니까?",
+            action: async () => {
+                const res = await axios.post('/users/update', user);
+                //console.log(res)
+                if (res.data === 0) {
+                    alert("수정 실패!");
+                } else {
+                    alert("수정 완료");
+                    //navi('/');
+                    console.log(users)
+                    console.log(user)
+                }
+            }
+        });
+    }
+
+    const getList = async () => {
+        setLoading(true);
+        const res = await axios.get(`/users/read.json/${sessionStorage.getItem("uid")}`);
+        //console.log(res.data)
+        setUsers(res.data);
+        //console.log(users)
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getList();
+    }, []);
+
+    if (loading) return <div className='text-center'><Spinner /></div>
+
+    return (
+        <div>
+            <Card className='update-card p-5'>
+                <h1 className='all-title'>정보 수정</h1>
+                <div className='update_img'>
+                    <img src='http://via.placeholder.com/150x150' onClick={() => img_ref.current.click()} style={{ cursor: 'pointer' }} width={300} height={300} />
+                    <input type='file' ref={img_ref} style={{ display: 'none' }} />
+                    <br />
+                    <Button className='update-img-btn'>이미지 수정</Button>
+                </div>
+                <InputGroup className='join-input'>
+                    <InputGroup.Text className='update-text'>닉네임</InputGroup.Text>
+                    <Form.Control value={nickname} name='nickname' type='text' onChange={onChange} />
+                </InputGroup>
+                <InputGroup className='join-input'>
+                    <InputGroup.Text className='update-text'>연락처</InputGroup.Text>
+                    <Form.Control value={phone} name='phone' type='text' onChange={onChange} />
+                </InputGroup>
+                <InputGroup className='join-address'>
+                    <InputGroup.Text className='update-text'>주소</InputGroup.Text>
+                    <Form.Control value={address1} name='address1' type='text' onChange={onChange} />
+                    <button className='update-btn'>검색</button>
+                </InputGroup>
+                <Form.Control value={address2} name='address2' className='join-input' type='text' placeholder='상세주소' onChange={onChange} />
+                <Form className='join-check'>
+                    <Form.Check label="꽃집 사장님"></Form.Check>
+                </Form>
+                <InputGroup className='join-input'>
+                    <InputGroup.Text className='update-text'>사업자 등록증</InputGroup.Text>
+                    <Form.Control />
+                    <button className='update-btn'>파일 검색</button>
+                </InputGroup>
+            </Card>
+            <div className='text-center'>
+                <Button className='mt-4 join-add' onClick={() => onSubmit()}>정보수정</Button>
+                <Button className='mt-4 join-add' type='reset'>취소</Button>
+            </div>
+        </div>
+    )
 }
 
 export default UserUpdate
--- src/component/mypage/detail/UserWithdrawal.jsx
+++ src/component/mypage/detail/UserWithdrawal.jsx
@@ -1,26 +1,54 @@
 import axios from 'axios';
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React, { useContext, useEffect, useState } from 'react'
+import { Button, Spinner } from 'react-bootstrap'
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { BoxContext } from '../../common/BoxContext';
 
 const UserWithdrawal = () => {
   const navi = useNavigate();
-  const { user_id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState([]);
+  const { box, setBox } = useContext(BoxContext);
+
+  const { user_id, uid, upass, uname, phone, address1, nickname } = user;
+
+  const getUser = async () => {
+    setLoading(true);
+    const res = await axios.get(`/users/read.json/${sessionStorage.getItem("uid")}`);
+    setUser(res.data);
+    setLoading(false);
+  }
 
   const onClickDelete = async (user_id) => {
-    if (window.confirm(`${user_id}을 삭제하시겠습니까?`)) {
-        console.log(user_id);
-        await axios.post(`/users/delete/${user_id}`);
-        navi(`/`);
-    }
-}
+    setLoading(true);
+    setBox({
+      show: true,
+      message: `'${uid}'을 삭제하시겠습니까?`,
+      action: async () => {
+        const res = await axios.post(`/users/delete/${user_id}`);
+        if (res.data === 0) {
+          alert("삭제 실패");
+        } else {
+          alert("회원 탈퇴가 되었습니다.");
+          sessionStorage.clear();
+          navi("/");
+        }
+      }
+    });
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (loading) return <div className='text-center my-5'><Spinner animation="border" variant="success" /></div>
 
   return (
     <div id="main_wrap">
       <h1 className="all-title">회원 탈퇴</h1>
       <div className="text-center">
-          <Button className='delete-btn' type="submit" onClick={()=>onClickDelete(user_id)}>탈퇴 신청</Button>
+        <Button className='delete-btn' type="submit" onClick={() => onClickDelete(uid)}>탈퇴 신청</Button>
       </div>
     </div>
   )
